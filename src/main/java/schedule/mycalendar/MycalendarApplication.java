7ec0223074c63ec3fc2bd4b2013f165b5eff878a
DESKTOP-VI6CUSJ\82107 <rlaalsxo1990@naver.com>
2021-10-25 23:23:38 +0900

    초기프로젝트 커밋

---
+++ src/main/java/schedule/mycalendar/MycalendarApplication.java
@@ -0,0 +1,13 @@
+package schedule.mycalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/schedule/mycalendar/MycalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package schedule.mycalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
