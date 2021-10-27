b0488e43ac947b958a4ff938b0a7ad378b0bd2bd
DESKTOP-VI6CUSJ\82107 <rlaalsxo1990@naver.com>
2021-10-27 22:19:14 +0900

    기본 세팅 및 설정 세팅

--- src/main/java/schedule/mycalendar/MycalendarApplication.java
+++ src/main/java/schedule/mycalendar/MycalendarApplication.java
@@ -1,13 +1,33 @@
 package schedule.mycalendar;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class MycalendarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycalendarApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
+	}
+
+	// @CreatedBy @LastModifiedBy 어노테이션으로 등록된 컬럼에 데이터를 생성하는 Bean
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
---
+++ src/main/java/schedule/mycalendar/entity/Member.java
@@ -0,0 +1,29 @@
+package schedule.mycalendar.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Builder
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
